0ea405924ea1a5a0a81e3b892080658b9eb99dc6
William-McKee <William.McKee.1201@gmail.com>
2017-12-22 12:05:42 -0500

    Add files via upload
    Listbox and scrollbar added - dummy data fill for now

--- septa_gui.py
+++ septa_gui.py
@@ -11,30 +11,24 @@ class Septa_Gui_Frame(tk.Frame):
     '''Septa Gui'''
     
     def __init__(self,parent=None):
-        '''Initialize Septa Gui'''
+        '''Build Septa Gui'''
         # Save initial grame
         self.root = tk.Frame.__init__(self,parent)
         self.parent = parent
         
-        # Initialize Radio Buttons
+        # Initial window settings
+        self.winfo_toplevel().title("SEPTA Data Exploration")
+        self.winfo_toplevel().geometry('300x300')
+        
+        # Radio Button Settings
         self.transport_choices = ["Bus", "Rail"]
         self.transport_selection = tk.IntVar()
         self.transport_selection.set(0)
         
-        # Build widgets
-        self.pack()
-        self.make_widgets()
-
-    def make_widgets(self):
-        '''Build Septa Gui'''
-        # Initial window settings
-        self.winfo_toplevel().title("SEPTA Data Exploration")
-        self.winfo_toplevel().geometry('300x80')
-        
-        # Label for transportation choices
-        tk.Label(self.root, 
-                 text="Select Transportation Type:",
-                 justify = tk.LEFT).pack()
+        # Create Label for transportation choices
+        self.septa_transport_label = tk.Label(self.root, 
+                                              text="Select Transportation Type:",
+                                              justify = tk.LEFT).pack()
         
         # Add transportation choices radio buttons
         for val, language in enumerate(self.transport_choices):
@@ -45,6 +39,33 @@ class Septa_Gui_Frame(tk.Frame):
                            command=self.ShowChoice,
                            value=val).pack(anchor=tk.W)
             
+        # Create Frame for Listbox            
+        self.septa_listbox_frame = tk.Frame(self.root)
+        self.septa_listbox_frame.pack()
+
+        # Create and link listbox and scrollbar
+        self.septa_listbox = tk.Listbox(self.septa_listbox_frame, width=20, height=10)
+        self.septa_listbox.pack(side="left", fill="y")
+        self.septa_scrollbar = tk.Scrollbar(self.septa_listbox_frame, orient="vertical")
+        self.septa_scrollbar.config(command=self.septa_listbox.yview)
+        self.septa_scrollbar.pack(side="right", fill="y")
+        self.septa_listbox.config(yscrollcommand=self.septa_scrollbar.set)
+
+        
     def ShowChoice(self):
-        '''Dummy function'''
-        print(self.transport_selection.get())
+        '''
+        Dummy function - how to populate list box
+        TODO: Add pandas dataframe contents to listbox in lieu of this dummy data
+        TODO: Pass directories into this class
+        '''
+        self.septa_listbox.delete(0, "end")
+        if (self.transport_selection.get() == 0):
+            lbitems = ["January", "February", "March", "April", "May", "June", 
+                       "July", "August", "September", "October", "November", "December"]
+            for item in lbitems:
+                self.septa_listbox.insert("end", item)
+        else:
+            lbitems = ["Sunday", "Monday", "Tuesday", "Wednesday",  
+                       "Thursday", "Friday", "Saturday"]
+            for item in lbitems:
+                self.septa_listbox.insert("end", item)
